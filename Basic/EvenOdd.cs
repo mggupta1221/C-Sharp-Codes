4a7924579968df5c98e0e6bf72d2c547e7bddb13
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-11 18:04:01 +0530

    EvenOdd

---
+++ Basic/EvenOdd.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BasicCodes
+{
+    public class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                {
+                    Console.WriteLine("Number enteres is 0");
+                }
+                else if (number % 2 == 0)
+                {
+                    Console.WriteLine("Number entered is Even");
+
+                }
+                else
+                {
+                    Console.WriteLine("Number entered is odd");
+
+                }
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
